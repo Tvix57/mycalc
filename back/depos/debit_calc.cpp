82d3463ee91a29c7fad70c4c57e25c4c1951c493
Audria Jhin <ajhin@student.21-school.ru>
2022-08-17 17:13:56 +0700

    new debit calc

--- back/depos/debit_calc.cpp
+++ back/depos/debit_calc.cpp
@@ -5,31 +5,64 @@
 Debit_calc::Debit_calc(double summ, double proc, double nalog , QDate start_date, QDate end_date) {
     this->summ = summ;
     this->proc = proc/100;
-    this->nalog = nalog;
+    this->nalog = nalog/100;
     this->start_date = start_date;
     this->end_date = end_date;
 }
 
-
-double Debit_calc::get_dayly_proc() {
-    double result;
-    result = (summ*proc*1/(QDate::currentDate().daysInYear()));
-    return result;
-}
-
-void Debit_calc::add_summ_on_bill(double addition)
-{
-    summ_on_bill += addition;
-}
-
 void Debit_calc::calculateNOcapit() {
+    if (addition_list.empty()) {
+        summ_on_bill = (summ * proc * (start_date.daysTo(end_date))) / start_date.daysInYear() /100;
+    } else {
 
+    }
 }
 
-void Debit_calc::calculate(int preiod) {
-
+void Debit_calc::calculate(int period) {
+    QDate capit_date = start_date;
+    capit_date = updateCapitDay(capit_date, period);
+    summ_on_bill = summ;
+    for (;start_date != end_date; start_date = start_date.addDays(1)) {
+        if (start_date == capit_date) {
+            capit_date = updateCapitDay(capit_date, period);
+            summ_on_bill += profit;
+            profit = 0;
+        }
+        if (start_date == addition_list.firstKey()) {
+            summ_on_bill += addition_list.value(start_date);
+        }
+      profit +=  round(summ_on_bill * proc * 1 / start_date.daysInYear()) / 100;
+    }
+    profit = summ_on_bill - summ;
+    if (nalog != 0) {
+        nalog_on_profit = profit * nalog;
+        profit -= nalog_on_profit;
+        summ_on_bill = summ + profit;
+    }
 }
 
 void Debit_calc::getNewAddition(QDate date, double summ) {
+    addition_list.insert(date, summ);
+}
 
+QDate Debit_calc::updateCapitDay(QDate date, int period) {
+    switch(period) {
+    case 0:date = date.addDays(1);
+        break;
+    case 1:date = date.addDays(7);
+        break;
+    case 2:date = date.addMonths(1);
+        break;
+    case 3:date = date.addMonths(3);
+        break;
+    case 4:date = date.addMonths(6);
+        break;
+    case 5:date = date.addYears(1);
+        break;
+    case 6:date = end_date;
+        break;
+    default:
+        break;
+    }
+    return date;
 }
--- back/depos/debit_calc.h
+++ back/depos/debit_calc.h
@@ -1,15 +1,12 @@
 #ifndef DEBIT_CALC_H
 #define DEBIT_CALC_H
 #include <QDate>
-#include <QSet>
+#include <QMap>
 
 class Debit_calc
 {
 public:
     Debit_calc(double, double, double, QDate, QDate);
-    double get_dayly_proc();
-
-    void add_summ_on_bill(double addition);
 
     double get_summ_on_bill() {return summ_on_bill;}
     double get_nalog() {return nalog;}
@@ -19,12 +16,14 @@ public:
     void getNewAddition(QDate, double);
 private:
     QDate start_date, end_date;
-    QSet <double> addition_list;
+    QMap <QDate,double> addition_list;
     double summ, proc, nalog;
 
     double summ_on_bill;
     double nalog_on_profit;
     double profit;
+
+    QDate updateCapitDay(QDate, int);
 };
 
 #endif // DEBIT_CALC_H
